d3d5145cc29502af483d77c8b12eb59d517a4a40
Recaiden <mw229@evansville.edu>
2015-11-30 23:57:06 -0500

    Fixed shuffle error.

--- physicallayer.c
+++ physicallayer.c
@@ -227,7 +227,7 @@ void shuffle(int *array, size_t n)
     for (i = 0; i < n - 1; i++)
     {
       //size_t j = i + rand() / (RAND_MAX / (n - i) + 1);
-      size_t j = i + rand() % n;
+      size_t j = rand() % n;
       int t = array[j];
       array[j] = array[i];
       array[i] = t;
